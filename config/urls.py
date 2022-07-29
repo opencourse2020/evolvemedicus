95507227fd151b4e722858cae8a23e8a3f007558
opencourse2020 <akaaboune@gmail.com>
2022-07-28 22:59:31 -0500

    first commit

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import url
 from django.views.generic import RedirectView
 from django.views.i18n import JavaScriptCatalog
 # from evolvemedicus.profiles.views import ProfileView
-
+from evolvemedicus.core import views
 admin.autodiscover()
 
 urlpatterns = [
@@ -16,7 +16,8 @@ urlpatterns = [
     # path("accounts/profile/", ProfileView.as_view()),
     # path("profiles/", include("hifuji.profiles.urls", namespace="profiles")),
     path("core/", include("evolvemedicus.core.urls", namespace="core")),
-    path("", RedirectView.as_view(pattern_name="core:dashboard")),
+    path("", views.HomeView.as_view())
+    # path("", RedirectView.as_view(pattern_name="core:dashboard")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
